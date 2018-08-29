986c9e1dcffc994dfcf99b478d16d12e662d116b
rherd <rob.a.herd@gmail.com>
2018-08-28 20:22:25 -0400

    schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,22 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+create database chat_n_dash;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+use chat_n_dash;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    user_pw varchar(255) not null,
+    last_chat TIMESTAMP 
+	);
+    
+create table if not exists favorites (
+	user_name varchar(255) not null,
+    favorites varchar(255) not null
+);
+
+create table chat (
+	user_1 varchar(255) not null,
+    user_2 varchar(255) not null,
+    msg varchar(1000) not null,
+    msg_time timestamp
+);
