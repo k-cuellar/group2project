72e4fd7ebac69a200e12dea02e298f75c84a97b8
rherd <rob.a.herd@gmail.com>
2018-09-06 18:23:11 -0400

    rooms

---
+++ models/room.js
@@ -0,0 +1,8 @@
+module.exports = function(sequelize, DataTypes) {
+    var Room = sequelize.define("Room", {
+      user_id1: DataTypes.INTEGER,
+      user_id2: DataTypes.INTEGER
+    });
+    return Room;
+  };
+  
