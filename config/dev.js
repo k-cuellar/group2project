0dc6ac4462756a7218e541e1da02e758243db04f
Andrew Park <apark11278@gmail.com>
2018-09-08 12:44:33 -0400

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-//dev.js DON'T COMMIT THIS
-module.exports={
-    cookieKey: 'alkfawovinawetha295sfhiuw48fhg',
-    googleClientID: '532556335282-6nkv895e6rra9kgdif15gc3u7ag403gu.apps.googleusercontent.com',
-    googleClientSecret: 'Jvypw4gYowiW8Xj1NEEZEA2E',
-}
